42544aa8831d9233df11a7c29ac82d31a7384487
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-28 17:06:24 +0100

    changed variable name

--- music_library/app.rb
+++ music_library/app.rb
@@ -1,15 +1,14 @@
 # file: app.rb
 
 require_relative 'lib/database_connection'
+require_relative 'lib/albums_repository'
+
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('music_library')
 
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
+album_repository = AlbumsRepository.new
 
-# Print out each record from the result set .
-result.each do |record|
-  p record
+album_repository.all.each do |album|
+  p album
 end
