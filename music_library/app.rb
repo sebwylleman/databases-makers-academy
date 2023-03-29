d828e41387ac7e37b245fb42adc69596683f0b20
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-29 14:46:31 +0100

    outputting find method query to console

--- music_library/app.rb
+++ music_library/app.rb
@@ -9,6 +9,10 @@ DatabaseConnection.connect('music_library')
 
 album_repository = AlbumsRepository.new
 
-album_repository.all.each do |album|
-  p album
-end
+# album_repository.all.each do |album|
+#   p album
+# end
+
+entry = album_repository.find(1)
+puts entry.title
+puts entry.release_year
