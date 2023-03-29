98673f5014240b3fedd6afb83ef004c1ff99d173
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-29 17:22:42 +0100

    printing values from recipes table

--- app.rb
+++ app.rb
@@ -1,15 +1,32 @@
-# file: app.rb
-
 require_relative 'lib/database_connection'
+require 'tty-table'  #to format as a table
+require_relative 'lib/recipe_repo'
 
-# We need to give the database name to the method `connect`.
 DatabaseConnection.connect('recipes_directory')
 
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM recipes;'
+sql = 'SELECT * FROM recipes;'
 result = DatabaseConnection.exec_params(sql, [])
 
-# Print out each record from the result set .
-result.each do |record|
-  p record
+# Create a table with headers
+headers = ['ID', 'Name', 'Cooking Time', 'Rating']
+
+# Create an array of rows using the result set
+rows = result.map do |record|
+  [record['id'], record['names'], record['cooking_time'], record['rating']]
 end
+
+# Create the table with headers and rows
+table = TTY::Table.new(headers, rows)
+
+# Render the table and print it
+puts table.render(:ascii)
+
+
+puts "Indivdual value for first recipe: "
+repo = RecipeRepo.new
+
+entry = repo.find(1)
+puts "recipe: #{entry.names}"
+puts "cooking time: #{entry.cooking_time}"
+puts "rating: #{entry.rating}"
+
