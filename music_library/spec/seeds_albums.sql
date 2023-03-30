12bad73272680f48a2edb6ec3590af07f8c5ca97
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-30 08:09:19 +0100

    removed unecessary files

--- music_library/spec/seeds_albums.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE TABLE artists RESTART IDENTITY; 
-TRUNCATE TABLE albums RESTART IDENTITY; 
-
-INSERT INTO artists (name, genre) VALUES ('The Beatles', 'rock');
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Abbey Road', 1969, 1);
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Revolver', 1966, 1);
